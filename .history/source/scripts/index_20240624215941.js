0e083de14248d27b00f88b3913162f17a3f97e8d
Insolent13 <frau.erlang1@gmail.com>
2024-06-24 22:31:15 +0300

    add adaptive graphics for catalog and form pages

---
+++ .history/source/scripts/index_20240624215941.js
@@ -0,0 +1,25 @@
+
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const orderButton = document.querySelector(".top-item__order");
+const orderPopup = document.querySelector(".modal-order");
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+
+orderButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  orderPopup.classList.add("modal-show");
+});
+
+
